75ef765442bb742ff50a97a102180e99a4458990
dbBurzum <khalidsr@code.edu.az>
2022-08-26 13:39:22 +0400

    feat get one fixed

--- InterviewTask/Mapper/MapperProfile.cs
+++ InterviewTask/Mapper/MapperProfile.cs
@@ -12,6 +12,13 @@ namespace InterviewTask.Mapper
             CreateMap<Employee, AllEmployeeDTO>()
                 .ForMember(e => e.Fullname, map => map.MapFrom(d => d.Fullname));
             CreateMap<Department, EmployeeDepartmentDTO>().ReverseMap();
+            CreateMap<Employee, EmployeeReturnDTO>()
+                .ForMember(e => e.Fullname, map => map.MapFrom(d => d.Fullname))
+                .ForMember(e => e.Birthdate, map => map.MapFrom(d => d.Birthdate))
+                .ForMember(e => e.Joined, map => map.MapFrom(d => d.CreateDate))
+                .ForMember(e => e.Department, map => map.MapFrom(d => d.Department));
+
+
             CreateMap<List<EmployeeReturnDTO>, EmployeeListDTO>()
                 .ForMember(d => d.Employees, map => map.MapFrom(s => s.Count))
                 .ForMember(d => d.Workers, map => map.MapFrom(s => s));
